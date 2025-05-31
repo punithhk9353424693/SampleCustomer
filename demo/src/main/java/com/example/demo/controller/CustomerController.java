e9c79c3d1ce4faa317135edf769c2b2d52d90a01
punithhk9353424693 <punithhk48@gmail.com>
2025-05-31 12:15:32 +0530

    new one

--- demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
@@ -18,17 +24,29 @@ public class CustomerController {
 	@Autowired
 	private CustomerService ser;
 	
-	@RequestMapping("/savecustomer")
+	@PostMapping("/savecustomer")
 	public ResponseEntity<String> saveCustomer(@RequestBody Customer customer){
 	     Customer customer2=  ser.saveCustomer(customer);
 	     return ResponseEntity.ok("saved Succesfully"+customer2);
 	
 	}
 	 
-	@RequestMapping("/getAllCustomers")
+	@GetMapping("/getAllCustomers")
 	public ResponseEntity<List<Customer>> getAllCustomers(){
 		List<Customer> customers=ser.getAllCustomers();
 		return ResponseEntity.ok(customers);
 		
+	}
+	@DeleteMapping("/deleteCustomer")
+	public ResponseEntity<String> deleteCustomer(@RequestParam Long id){
+		String s=ser.deleteCustomer(id);
+		return ResponseEntity.ok(s);
+		
+	}
+	@PutMapping("/updateCustomer")
+	public ResponseEntity<String> updateCustomer(@RequestParam Long id,@RequestBody Customer customer){
+		
+		String s=ser.updateCustomer(id, customer);
+		return ResponseEntity.ok(s);
 	}
 }
